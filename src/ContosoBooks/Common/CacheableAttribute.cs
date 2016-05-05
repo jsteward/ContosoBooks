ee3504da5a738535e8fbb6db407e8a56a1ae719a
jsteward <jacques.steward@gmail.com>
2016-05-05 14:42:40 -0500

    added CacheableAttribute

---
+++ src/ContosoBooks/Common/CacheableAttribute.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc.Filters;
+
+namespace ContosoBooks.Common
+{
+    [AttributeUsage(AttributeTargets.Method)]
+    public class CacheableAttribute : ActionFilterAttribute
+    {
+        
+
+        public CacheableAttribute()
+        {
+            
+        }
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            
+            base.OnActionExecuting(context);
+        }
+
+        public override void OnActionExecuted(ActionExecutedContext context)
+        {
+            base.OnActionExecuted(context);
+        }
+    }
+}
